e14a411e3f4d97d7f21f28a43e2e59ab88bf09d5
Vadim <mail@vadimklimenko.com>
2014-11-29 00:02:19 +0200

    Turn on LED if photovalue < 400

--- index.js
+++ index.js
@@ -8,18 +8,32 @@ app.use(express.static('public'));
 
 var board = new Board("/dev/tty.usbmodem1411");
 
+var photoresistorValue = 0;
+
+board.on("ready", function() {
+	var photoresistorPin = 0;
+	var ledPin = 13;
+
+	board.pinMode(ledPin, board.MODES.OUTPUT);
+	board.digitalWrite(ledPin, board.LOW);
+	
+	board.analogRead(0,function(val) {
+		if(val < 400) {
+			board.digitalWrite(ledPin, board.HIGH);
+		} else {
+			board.digitalWrite(ledPin, board.LOW);
+		};
+
+		photoresistorValue = val;
+	})
+});
 
 io.on('connection', function (socket) {
-	board.on("ready", function() {
-		var data = 0;
-		board.analogRead(0,function(val) {
-			data = val;
-		})
-		setInterval(function() {
-			// console.log(data);
-			socket.broadcast.emit('analogValue', data);
-		}, 100);
-	});
+
+	setInterval(function() {
+		// console.log(photoresistorValue);
+		socket.broadcast.emit('analogValue', photoresistorValue);
+	}, 100);
 });
 
 
